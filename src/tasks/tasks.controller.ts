4a99c3222bc655eca2ce05bac0d5983b2af4d8f3
zrcoy <zhangruichaotyler@gmail.com>
2023-11-16 15:33:29 -0500

    feat: add createTask service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+
+import { Task } from "./task.model";
 import { TasksService } from "./tasks.service";
 
 @Controller("tasks")
@@ -6,7 +8,15 @@ export class TasksController {
   public constructor(private tasksService: TasksService) {}
 
   @Get()
-  public getAllTasks() {
+  public getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  public createTask(
+    @Body("title") title: string,
+    @Body("description") description: string
+  ): Task {
+    return this.tasksService.createTask(title, description);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from "@nestjs/common";
+import { v4 as uuidv4 } from "uuid";
+import { Task, TaskStatus } from "./task.model";
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
-  public getAllTasks() {
+  public getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  public createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaskStatus.OPEN
+    };
+
+    this.tasks.push(task);
+
+    return task;
   }
 }
